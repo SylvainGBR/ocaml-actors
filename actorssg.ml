c1658b22644f6eefef7218d87e1d733fe3fc23fc
SylvainGBR <sylvabry@hotmail.com>
2012-04-24 17:23:52 +0200

    correction mutex dans reacting + ajout debug + test ping pong

--- actorssg.ml
+++ actorssg.ml
@@ -1,3 +1,5 @@
+let debug = false;;
+
 type arg =
   | Actor of actor
 
@@ -65,6 +67,16 @@ let actors = Hashtbl.create 1313 (* Should probably be a weak hashtbl *)
 
 (* let machines = Hashtbl.create 97 *)
 
+let mutex_lock mut =
+  if debug then print_string "Locking. ";
+  Mutex.lock mut;
+  if debug then print_string "Locked. ";;
+
+let mutex_unlock mut = 
+  if debug then print_string "Unlocking. ";
+  Mutex.unlock mut; 
+  if debug then print_string "Unlocked. \n";;
+
 let actors_id = ref 0
 let a_mutex = Mutex.create()
 
@@ -72,22 +84,31 @@ let receive_scheduler = Queue.create()
 let rs_mutex = Mutex.create()
 
 let schedule_receive a f =
-  Mutex.lock rs_mutex;
+  if debug then print_string "In Schedule_receive : ";
+  mutex_lock rs_mutex;
   Queue.add (a, f) receive_scheduler;
-  Mutex.unlock rs_mutex;;
+  mutex_unlock rs_mutex;;
 
 let awake aid =
+  if debug then print_string "In Awake : ";
   let a_env = Hashtbl.find actors aid in
-  try 
-    let f = Queue.pop a_env.sleeping in
-    schedule_receive a_env.actor f
-  with Queue.Empty -> ();;
+  match a_env.actor.actor_location with
+    | Local lac ->
+        begin mutex_lock lac.mutex; 
+          try
+            let f = Queue.pop a_env.sleeping in
+            begin schedule_receive a_env.actor f;
+              mutex_unlock lac.mutex end
+          with Queue.Empty -> mutex_unlock lac.mutex
+        end
+    | Remote o -> failwith "You cannot awake a remote actor";;
 
 let send a m =
   match a.actor_location with
-    | Local lac -> begin Mutex.lock lac.mutex;
+    | Local lac -> begin if debug then print_string "In Send : ";
+      mutex_lock lac.mutex;
       Queue.add (mutables_copy m) lac.mailbox;
-      Mutex.unlock lac.mutex; 
+      mutex_unlock lac.mutex; 
       awake a.actor_id end
     | Remote o -> ();;
 
@@ -95,49 +116,73 @@ exception React of (message -> unit);;
 
 exception NotHandled;;
 
-let execute a f =
+let react f = raise (React f);;
+
+let start a f =
   try f()
   with React g -> schedule_receive a g;;
 
-let new_aid() =
-  Mutex.lock a_mutex;
-  incr actors_id;
-  let i = !actors_id in
-  begin Mutex.unlock a_mutex;
-    i end;;
-
-let create f =
-  let i = new_aid() in
+let create() =
+  let new_aid() =
+    Mutex.lock a_mutex;
+    incr actors_id;
+    let i = !actors_id in
+    begin Mutex.unlock a_mutex;
+      i end in
+  let id = new_aid() in
   let l_act = {mailbox = Queue.create() ; mutex = Mutex.create()} in
-  let new_actor = {actor_id = i; actor_location = Local l_act} in
+  let new_actor = {actor_id = id; actor_location = Local l_act} in
   let new_act_env = {actor = new_actor; sleeping = Queue.create()} in
-  Hashtbl.add actors new_actor.actor_id new_act_env; 
-  execute new_actor f;
+  Hashtbl.add actors new_actor.actor_id new_act_env;
   new_actor;;
 
-let rec reacting a g =
+let reacting a g =
   match a.actor_location with
-    | Local lac -> begin Mutex.lock lac.mutex;
+    | Local lac -> begin if debug then print_string "In Reacting : ";
+      mutex_lock lac.mutex;
       let rec reacting_aux() = 
         try
           let m = Queue.pop lac.mailbox in
-          try (g m)
+          try 
+            mutex_unlock lac.mutex; g m;
           with 
-            | React f -> begin  Mutex.unlock lac.mutex;
-                reacting a f end
-            | NotHandled -> reacting_aux();
-        with Queue.Empty -> let a_env = Hashtbl.find actors a.actor_id in
-                             Queue.add g a_env.sleeping;
-      in
-      reacting_aux(); end
+            | React f -> begin print_newline();
+              schedule_receive a f end
+            | NotHandled ->  begin mutex_unlock lac.mutex;
+              reacting_aux() end
+        with Queue.Empty -> let a_env = Hashtbl.find actors a.actor_id in 
+                            begin Queue.add g a_env.sleeping;
+                              mutex_unlock lac.mutex end
+      in reacting_aux() end
     | Remote rac -> failwith "You cannot run a remote actor";;
   
-let recieve_handler() = 
-  try 
-    let (a, f) = Queue.pop receive_scheduler in reacting a f;
-  with Queue.Empty -> Thread.delay 0.01;;
+let rec recieve_handler() = 
+  if debug then print_string "RH : number ";
+  (try 
+     let (a, f) = Queue.pop receive_scheduler in 
+     begin  if debug then Printf.printf "%d \n" a.actor_id;
+       reacting a f end
+   with Queue.Empty -> Thread.delay 0.01);
+  recieve_handler();;
 
-
-(*val create : (actor -> unit) -> actor
-val receive : actor -> (message -> unit) -> unit
-val send : actor -> msg -> unit*)
+let act1 = create() in
+let act2 = create() in
+let rec ping() =
+  react pig
+and pig m = 
+  let (st1, l) = m in 
+  if (st1 = "ping") then begin print_string "ping\n"; 
+    send act2 ("pong", (I 1) :: []);
+    ping() end in
+let rec pong() =
+  react pog
+and pog m = 
+  let (st2, l) = m in 
+  if (st2 = "pong") then begin print_string "pong\n"; 
+    send act1 ("ping", (I 1) :: []);
+    pong() end in begin
+      start act1 ping;
+      start act2 pong;
+      send act1 ("ping", (I 1) :: []);
+      recieve_handler();
+    end;;
